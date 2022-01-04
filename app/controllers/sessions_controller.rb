68041d2f9b98dcb6ecd80341c806c3f48fa3b482
Samantha Diaz <samidiaz3@gmail.com>
2022-01-03 23:24:26 -0500

    fixed logged out routes and link on home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,18 +4,28 @@ class SessionsController < ApplicationController
     end
 
     def create
+        puts params
+        puts "HA HA HA HA HA HA"
         user = User.find_by(email: params[:email])
         # return head(:forbidden) unless @user.authenticate(params[:password])
         user = user.try(:authenticate, params[:password])
         return redirect_to(controller: 'sessions', action: 'new') unless user
         session[:user_id] = user.id
         @user = user
-        redirect_to controller: 'static', action: 'home' #appointments
+        redirect_to controller: 'static', action: 'about' #appointments -- need to change this
+
+
+        # Parameters: {"authenticity_token"=>"[FILTERED]", "session"=>{"email"=>"sam@email.com", "password"=>"[FILTERED]"}, "commit"=>"Login"}
+        # {"authenticity_token"=>"YQNvTJ...2URQ", "session"=>{"email"=>"sam@email.com", "password"=>"123"}, "commit"=>"Login", "controller"=>"sessions", "action"=>"create"}
     end
 
     def destroy
-        session.delete :user_id
-        redirect_to '/'
+        if logged_in?
+            session.delete :user_id
+            redirect_to '/about'
+        else
+            redirect_to '/contact'
+        end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,8 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/login', to: 'sessions#destroy'
-
-
+  get '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy'
 
 end
 
