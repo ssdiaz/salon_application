fd1919991dbe37cb4fe8f23f0f35748760839931
Samantha Diaz <samidiaz3@gmail.com>
2022-01-13 13:20:29 -0500

    more styling edits in the views templates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,6 @@ class ApplicationController < ActionController::Base
   
   include SessionsHelper
 
+  include ActionView::Helpers::NumberHelper
+
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -6,4 +6,5 @@ class Client < ApplicationRecord
     has_many :stylists, through: :appointments
 
     validates_presence_of :name, :email, :phone
+    validates :phone, length: { is: 10 }
 end
